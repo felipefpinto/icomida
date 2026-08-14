12680cb7bcc96861f256413d4562e5aa6508f295
Felipe Pinto <felipefernandespinto1093@gmail.com>
2026-08-14 11:32:21 -0300

    (feat/style) - Criação da Pagina Principal

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,27 @@
-// src/app/restaurante/page.js
+import Header from "./components/Header";
 
-export default function RestaurantePage() {
+export default function Home() {
   return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold">Página do Restaurante</h1>
-      <p className="mt-2 text-gray-600">
-        Aqui vai o cardápio e as informações do estabelecimento.
-      </p>
-    </main>
+    <div className="min-h-screen bg-gray-50">
+
+      <Header />
+
+      <main>
+
+        <section className="mx-auto max-w-[1400px] px-6 py-10">
+
+          <h1 className="text-4xl font-bold text-gray-900">
+            Encontre seu restaurante favorito
+          </h1>
+
+          <p className="mt-3 text-gray-500">
+            Peça sua comida favorita onde estiver.
+          </p>
+
+        </section>
+
+      </main>
+
+    </div>
   );
 }
